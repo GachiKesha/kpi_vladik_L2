7edd4133aecb7126a3e6bb3d94232e2c41c2ea44
GachiKesha <148109167+GachiKesha@users.noreply.github.com>
2024-10-24 20:21:07 +0000

    C0

---
+++ donk.h
@@ -0,0 +1,13 @@
+#ifndef DONK_H
+#define DONK_H
+
+#include <cmath>
+
+class Lab2 {
+public:    
+    double FuncA() {
+        return std::sin(0);
+    }
+};
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "donk.h"
+
+int main() {
+    Lab2 lab;
+    double result = lab.FuncA();
+    std::cout << result << std::endl;
+    return 0;
+}
