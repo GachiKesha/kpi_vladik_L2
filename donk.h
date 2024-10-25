8a8dd0fb4e0895551083f6f781b5c5869d471fd3
VladUbuntu <stukalin.vlad@gmail.com>
2024-10-25 23:29:24 +0000

    C3

--- donk.h
+++ donk.h
@@ -8,21 +8,34 @@
 
 
 class Lab2 {
+/**
+ * Class representing mathematical operations.
+ */
 public: 
-    double FuncA(int n) {
-        double x;
-
-        std::cout << "Enter the value of x (in radians): ";
-        std::cin >> x;
-        
+    /**
+      * Returns value of Taylor series approximation for tan(x).
+      * 
+      * @param x The angle in radians for which to compute tan(x).
+      * @param n The number of terms to include in the Taylor series approximation.
+      * @return taylorTan(x, n).
+      */
+    double FuncA(int x, n) {      
         return taylorTan(x, n);
     };
 
-private:        
-    std::unordered_map<int, double> bernoulliCache;
+private:
+    // Maps and vector to cache used values
+    std::unordered_map<int, double> bernoulliCache; 
     std::unordered_map<std::string, int> binomialCache;
     std::vector<double> factorialCache = {1, 1};
     
+    /**
+      * Implementation of Taylor series approximation for tan(x)
+      * 
+      * @param x The angle in radians for which to compute tan(x). Value is adjusted to be in range [0;Pi/2]
+      * @param n The number of terms to include in the Taylor series approximation.
+      * @return The approximate value of tan(x) computed using the specified number of terms.
+      */
     double taylorTan(double x, int terms) {
         x = std::fmod(x, M_PI);
         if (x > M_PI / 2) x -= M_PI;
@@ -30,12 +43,11 @@ private:
 
         double sum = 0.0;
 
-        for (int n = 0; n < terms; n++) {
+        for (int n = 0; n < terms; n++) {            
             double term = getBernoulliNumber(2 * n) * std::pow(-4, n)
             * (1 - std::pow(4, n)) * std::pow(x, 2 * n - 1) / getFactorial(2 * n);
 
             if (std::isinf(term) || std::isnan(term)) {
-                std::cout << term << "-err" << std::endl;
                 break;
             }
 
@@ -44,7 +56,13 @@ private:
 
         return sum;
     };
-
+    
+    /**
+      * Computes Bernoulli numbers up to B_n using a specific recursive formula.
+      * 
+      * @param n The maximum index of the Bernoulli numbers to compute.
+      * @return A vector containing the computed Bernoulli numbers from B_0 to B_n.
+      */
     std::vector<double> bernoulliNumbers(int n) {
         std::vector<double> B(n + 1, 0.0);
         B[0] = 1.0;
@@ -58,6 +76,12 @@ private:
         return B;
     };
 
+    /**
+      * Retrieves the nth Bernoulli number, calculating it if not already cached.
+      * 
+      * @param n The index of the Bernoulli number to retrieve.
+      * @return The nth Bernoulli number.
+      */
     double getBernoulliNumber(int n) {
         if (bernoulliCache.find(n) != bernoulliCache.end()) {
             return bernoulliCache[n];
@@ -66,7 +90,14 @@ private:
         bernoulliCache[n] = B[n];
         return B[n];
     };
-
+    
+    /**
+      * Computes the binomial coefficient C(n, k) = n! / (k! * (n - k)!).
+      * 
+      * @param n The total number of items.
+      * @param k The number of items to choose.
+      * @return The binomial coefficient C(n, k).
+      */
     int getBinomialCoefficient(int n, int k) {
         std::string key = std::to_string(n) + "," + std::to_string(k);
         if (binomialCache.find(key) != binomialCache.end()) {
@@ -83,7 +114,13 @@ private:
         binomialCache[key] = res;
         return res;
     };
-
+    
+    /**
+      * Computes the factorial of n, caching results to improve efficiency.
+      * 
+      * @param n The number for which to compute the factorial.
+      * @return The factorial of n (n!).
+      */
     double getFactorial(int n) {
         while (n >= factorialCache.size()) {
             int nextIndex = factorialCache.size();
--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 int main() {
     Lab2 lab;
     int n;
+    double x;
+    std::cout << "Enter the value of x (in radians): ";
+    std::cin >> x;
     std::cout << "Enter the number of terms in the series: ";
     std::cin >> n;
-    double result = lab.FuncA(n);
+    double result = lab.FuncA(x, n);
     std::cout << result << std::endl;
     return 0;
 }
